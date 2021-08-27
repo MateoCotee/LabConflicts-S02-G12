d11ad2d5328a94f46292fb6f3b69ae4c19b52189
Diego Acosta Corredor <da.acostac1@uniandes.edu.co>
2021-08-27 16:21:26 -0500

    Cambios del estudiante 1

--- App/view.py
+++ App/view.py
@@ -43,7 +43,6 @@ def printMenu():
     print("1- Cargar Libros")
     print("2- Cargar Tags")
     # TODO: Modificación de Est-1 en el Lab 2
-    print("3- Cargar Book-Tags")
     print("0- Salir")
 
 
